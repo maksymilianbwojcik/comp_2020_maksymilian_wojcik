78fe9e16f806cb5415e6e0019c030f9df96da5d3
Maksymilian Wójcik <maksymilian.b.wojcik@gmail.com>
2020-11-13 11:07:45 +0100

    02.1 Working initial steps

---
+++ 02_entity_framework_core/02_exercise/project/App/Migrations/20201113081937_Initial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace App.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder?.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    CreationDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder?.DropTable(
+                name: "Projects");
+        }
+    }
+}
--- 02_entity_framework_core/02_exercise/project/App/Program.cs
+++ 02_entity_framework_core/02_exercise/project/App/Program.cs
@@ -1,8 +1,26 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Utils;
 
 namespace App
 {
+    public class ProjectContext : DbContext
+    {
+
+        #region why does it work in jupyter
+        // public DbSet<Project> Projects { get; set; }
+        // https://docs.microsoft.com/en-us/ef/core/miscellaneous/nullable-reference-types#dbcontext-and-dbset
+        #endregion
+        public DbSet<Project> Projects => Set<Project>();
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=/home/student/comp_2020_maksymilian_wojcik/02_entity_framework_core/02_exercise/project/App/blog.db");
+        } 
+    }
+    
     [ExcludeFromCodeCoverage]
     internal static class Program
     {
@@ -10,7 +28,19 @@ namespace App
         {
             // TODO: Create and use DB context...
             
-            Console.WriteLine("Hello World!");
+            using (var db = new ProjectContext())
+            {
+                var project = new Project { CreationDate = DateTime.Now };
+                db.Projects.Add(project);
+                db.SaveChanges();
+            }
+
+            using (var db = new ProjectContext())
+            {
+                var projects = db.Projects
+                    .ToList();
+            }
         }
     }
+
 }
