4c3715bd4c6551ae01411e98f051332e03488a8c
Maksymilian Wójcik <maksymilian.b.wojcik@gmail.com>
2020-11-19 15:18:22 +0100

    Added tests to WeatherForecast

--- 03_rest_api/02_exercise/project/Test/UnitTest1.cs
+++ 03_rest_api/02_exercise/project/Test/UnitTest1.cs
@@ -7,11 +7,28 @@ namespace Test
     public class UnitTest1
     {
         [Fact]
-        public void IsFahrenheitedConvertedRight()
+        public void IsFahrenheitConvertedRight()
         {
-            var weatherForecast = new WeatherForecast();
-            weatherForecast.TemperatureC = 0;
+            var weatherForecast = new WeatherForecast {TemperatureC = 0};
             Assert.Equal(32, weatherForecast.TemperatureF);
+        }
+
+        [Fact]
+        public void IsDateCorrect()
+        {
+            var weatherForecast = new WeatherForecast
+            {
+                Date = DateTime.Today,
+                TemperatureC = 100,
+            };
+            Assert.Equal(DateTime.Today, weatherForecast.Date);
+            Assert.Equal(100, weatherForecast.TemperatureC);
+            Assert.Equal(211, weatherForecast.TemperatureF);
+            Assert.Null(weatherForecast.Summary);
+
+            weatherForecast.Summary = "no kiepsko";
+            
+            Assert.Equal("no kiepsko", weatherForecast.Summary);
         }
     }
 }
---
+++ 03_rest_api/02_exercise/project/Test/WeatherForecastControllerTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Test
+{
+    public class WeatherForecastControllerTest
+    {
+        [Fact]
+        public void IsRangeCorrect()
+        {
+            // Assert.Collection(new );
+        }
+    }
+}
