529f995fa57b52112a4a3bc2f1a214b6dc57a9eb
Maksymilian Wójcik <maksymilian.b.wojcik@gmail.com>
2020-11-25 18:59:42 +0100

    05.1 Creation of Client and Server

---
+++ 05_grpc/02_exercise/project/Client/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Client
+{
+    class Program
+    {
+        // static void Main(string[] args)
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
