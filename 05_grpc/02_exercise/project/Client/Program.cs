71717ebb6e6794e1befafa6ad330c4ac00449612
Maksymilian Wójcik <maksymilian.b.wojcik@gmail.com>
2020-11-26 04:30:13 +0100

    05.1 Working communication between Client and Server

--- 05_grpc/02_exercise/project/Client/Program.cs
+++ 05_grpc/02_exercise/project/Client/Program.cs
@@ -1,13 +1,26 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
 
-namespace Client
+namespace GrpcGreeterClient
 {
     class Program
     {
-        // static void Main(string[] args)
-        static void Main()
+        static async Task Main() // string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var httpHandler = new HttpClientHandler();
+
+            httpHandler.ServerCertificateCustomValidationCallback = 
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001",
+                new GrpcChannelOptions { HttpHandler = httpHandler });
+                
+            var client =  new Greeter.GreeterClient(channel);
+            var request = new HelloRequest { Name = "GreeterClient" };
+            var reply = await client.SayHelloAsync(request);
+            Console.WriteLine("Greeting: " + reply.Message);
         }
     }
 }
