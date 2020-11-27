7645f42f91d66c6e00473a8889c0a98bdc963a31
Maksymilian Wójcik <maksymilian.b.wojcik@gmail.com>
2020-11-27 05:11:38 +0100

    06.1 Created project Gui

---
+++ 06_avalonia/02_exercise/project/Gui/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gui.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}
